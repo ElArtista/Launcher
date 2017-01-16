6a8a752dfe25df4755ad00013d1d0ac42374adbf
TheArtist <agorglouk@gmail.com>
2017-01-16 22:21:36 +0200

    Added Linux window implementation

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ LIBS = macu glad
 ifeq ($(OS), Windows_NT)
 	LIBS += glu32 opengl32 msimg32 gdi32 user32
 else
-	LIBS += pthread dl
+	LIBS += GL X11 Xrender pthread dl
 endif
 EXTDEPS = glad::0.1.12a0 macu::0.0.2dev
